5c26c804acdc2d7a74cfb1ae72883199eac508b2
Bojan Rajh <b.rajh@shopware.com>
2023-09-26 10:44:38 +0200

    fix: tmp

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -1,15 +1,11 @@
 import { defineConfigWithTheme } from "vitepress";
-import type { HeadConfig, TransformContext } from 'vitepress'
 import type { Config as ThemeConfig } from "vitepress-shopware-docs";
 import baseConfig from "vitepress-shopware-docs/config";
 import ViteRequireContext from '@originjs/vite-plugin-require-context'
-const {resolve} = require('path');
-//import { MarkdownTransform } from "../plugins/markdownTransform";
+import {resolve} from "path";
 import Inspect from "vite-plugin-inspect";
 
-import {addOGImage, copyAdditionalAssets, createSitemap, storeRedirects} from "../node_modules/@shopware-docs/vitepress/src/helpers";
-//import {generateMarkdownFromStoplight, getStoplightUrls} from "../node_modules/@shopware-docs/vitepress/src/helpers/stoplight";
-import {generateMarkdownFromStoplight, getStoplightUrls} from "@shopware-docs/vitepress/helpers/stoplight";
+import {createSitemap, storeRedirects} from "./helpers";
 import navigation from "./navigation";
 
 import {
@@ -81,10 +77,9 @@ export default defineConfigWithTheme<ThemeConfig>({
     mode: 'development',
     plugins: [
       Inspect(),
-      ViteRequireContext({
+      ViteRequireContext.default({
         projectBasePath: `${process.cwd()}/src`
       }),
-      //MarkdownTransform(),
     ],
     build: {
       rollupOptions: {
@@ -101,71 +96,15 @@ export default defineConfigWithTheme<ThemeConfig>({
     reactivityTransform: resolve(__dirname, 'src'), // true
   },
 
-  async transformHead(context: TransformContext): Promise<HeadConfig[]> {
-    const head: HeadConfig[] = [];
-
-    addOGImage(head, context);
-
-    return head;
-  },
-
   async buildEnd() {
-    /**
-     * Copy additional assets not present in the assets or public dir.
-     */
-    await copyAdditionalAssets([
-      {
-        src: '../node_modules/@shopware-ag/meteor-icon-kit/icons/regular',
-        dst: 'icons/regular',
-      },
-      {
-        src: '../node_modules/@shopware-ag/meteor-icon-kit/icons/solid',
-        dst: 'icons/solid',
-      }
-    ]);
-
     /**
      * Copy redirects.
      */
     await storeRedirects();
 
     /**
-     * Fetch Stoplight URLs.
-     */
-    /*const stoplightUrls = [
-      ...await getStoplightUrls({
-        source: 'https://shopware.stoplight.io/api/v1/projects/cHJqOjEwNjA0NQ/table-of-contents',
-        prefix: '/resources/api/store-api-reference.html#/',
-      }),
-      ...await getStoplightUrls({
-        source: 'https://shopware.stoplight.io/api/v1/projects/cHJqOjEwNjA0Mw/table-of-contents',
-        prefix: '/resources/api/admin-api-reference.html#/',
-      }),
-    ];*/
-
-    /**
      * Create public sitemap.xml.
      */
-    await createSitemap(/*stoplightUrls*/);
-
-    /**
-     * Transform Store API JSON reference to Markdown for Knowledge base.
-     */
-    /*await generateMarkdownFromStoplight({
-      source: 'https://shopware.stoplight.io/api/v1/projects/cHJqOjEwNjA0NQ/table-of-contents',
-      nodes: 'https://shopware.stoplight.io/api/v1/workspaces/d2s6MzM5MTQ/nodes?project_ids[0]=cHJqOjEwNjA0NQ',
-      reference: 'https://raw.githubusercontent.com/shopware/store-api-reference/main/storeapi.json',
-      as: 'resources/api/admin-api-reference.html',
-    });*/
-
-    /**
-     * Transform Admin API JSON reference to Markdown for Knowledge base.
-     */
-    /*await generateMarkdownFromStoplight({
-      source: 'https://shopware.stoplight.io/api/v1/projects/cHJqOjEwNjA0Mw/table-of-contents',
-      nodes: 'https://shopware.stoplight.io/api/v1/workspaces/d2s6MzM5MTQ/nodes?project_ids[0]=cHJqOjEwNjA0Mw',
-      reference: 'https://raw.githubusercontent.com/shopware/admin-api-reference/main/storeapi.json',
-      as: 'resources/api/store-api-reference.html',
-    }, false);*/
+    await createSitemap();
   }
 });
