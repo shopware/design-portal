df1474c5bafdaf514c5d0f61f462dd8be55f8c9e
alastair-simon <alastairfraser.work@gmail.com>
2025-02-06 10:51:10 +0100

    feat: Add token pages
    feat: update config to include token page
    
    feat: add token page color
    
    feat: add sample tokens
    
    fix: editor sidebar to display correct token name
    
    chore: add meteor/tokens
    
    feat: add token sorting
    
    refactor: remove import form component
    
    add css tokens import
    
    add border and font pages
    
    feat: add markdown table
    
    feat: add swatch component
    
    feat: replace markdown with swatch component
    
    feat: add border to swatch
    
    feat: add font token preview component
    
    feat: add weight prop
    
    adjust font weight preview
    
    feat: adjust table styling
    
    feat: add missing tokens
    
    feat: add lineHeight prop
    
    feat: add border tokens
    
    feat: add missing color tokens
    
    feat: cleanup tokens pages

---
+++ src/components/tokens/SwagTokensBorderPreview.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="swag-tokens-border-preview">
+        <div class="border-preview" :style="{borderRadius: borderRadius}"></div>
+    </div>
+</template>
+
+<script lang="ts">
+export default {
+    props: {
+        borderRadius: {
+            type: String,
+            required: true,
+        },
+    },
+};
+</script>
+
+<style scoped>
+.swag-tokens-font-preview {
+    min-height: 40px;
+    width: 100%;
+    text-align: left;
+}
+.border-preview {
+    width: 50px;
+    height: 50px;
+    background: #2D2E32;
+}
+</style>
