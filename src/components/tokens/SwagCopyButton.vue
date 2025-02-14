69f6b2ebeabab7ab2bfe9d9658d87a4ca3746da8
alastair-simon <alastairfraser.work@gmail.com>
2025-02-14 17:47:35 +0100

    feat: create SwagCopyButton

---
+++ src/components/tokens/SwagCopyButton.vue
@@ -0,0 +1,68 @@
+<template>
+  <button @click="copyToClipboard" class="swag-button">
+    <SwagIcon
+      type="regular"
+      icon="checkmark"
+      v-if="copied"
+      class="icon-check"
+    />
+    <SwagIcon type="regular" icon="copy" v-else class="icon-copy" />
+  </button>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const props = defineProps({
+  textToCopy: {
+    type: String,
+    required: true,
+  },
+  buttonText: {
+    type: String,
+    default: "Copy",
+  },
+});
+
+const copied = ref(false);
+
+const copyToClipboard = async () => {
+  try {
+    await navigator.clipboard.writeText(props.textToCopy);
+    copied.value = true;
+    setTimeout(() => {
+      copied.value = false;
+    }, 2000);
+  } catch (err) {
+    console.error("Failed to copy: ", err);
+  }
+};
+</script>
+
+<style scoped>
+.swag-button {
+  width: 30px !important;
+  height: 30px !important;
+  padding: 1px;
+  cursor: pointer;
+  border: none;
+  border-radius: 5px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  border: 1px solid red;
+}
+
+.swag-button .icon-check {
+  width: 16px;
+}
+
+.swag-button .icon-copy {
+  width: 16px;
+}
+
+.swag-button:hover {
+  background: rgb(0, 123, 255);
+}
+</style>
