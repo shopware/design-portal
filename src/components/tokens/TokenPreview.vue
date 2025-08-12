9f1d02871b1a178d4ca7b31db0002746234f30c0
alastair-simon <alastairfraser.work@gmail.com>
2025-08-12 12:29:25 +0200

    refactor: text size

--- src/components/tokens/TokenPreview.vue
+++ src/components/tokens/TokenPreview.vue
@@ -2,8 +2,9 @@
   <div
     class="w-full flex flex-row justify-between border-t-0 border-l-0 border-r-0 border-b-1 border-solid border-[#091E42]/14"
   >
-    <div class="m-16px">
+    <div class="flex flex-col m-t-16px gap-2 text-sm">
       <slot name="tokenName" />
+      <slot name="tokenDescription" />
     </div>
     <div class="flex flex-row justify-between">
       <slot name="lightToken" />
